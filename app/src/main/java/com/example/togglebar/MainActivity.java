342bd265b70c2ddebdefe2b11f5f46ce4f16392e
Nirendra <nirendra@gmail.com>
2020-01-05 05:56:42 +0530

    First checkin

---
+++ app/src/main/java/com/example/togglebar/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.togglebar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    ToggleButton simpleToggleButton1, simpleToggleButton2;
+    Button submit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // initiate toggle button's
+        simpleToggleButton1 = (ToggleButton) findViewById(R.id.simpleToggleButton1);
+        simpleToggleButton2 = (ToggleButton) findViewById(R.id.simpleToggleButton2);
+        submit = (Button) findViewById(R.id.submitButton);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String status = "ToggleButton1 : " + simpleToggleButton1.getText() + "\n" + "ToggleButton2 : " + simpleToggleButton2.getText();
+                Toast.makeText(getApplicationContext(), status, Toast.LENGTH_SHORT).show(); // display the current state of toggle button's
+            }
+        });
+    }
+
+
+}
